dd33ad8bc7b8abbe6043a24bb2a4aa528ce68b5a
LimJiNa <jn4624@gmail.com>
2024-04-15 23:12:53 +0900

    MyBatis - MyBatis 사용

---
+++ src/main/java/hello/item/repository/mybatis/ItemMapper.java
@@ -0,0 +1,21 @@
+package hello.item.repository.mybatis;
+
+import hello.item.domain.Item;
+import hello.item.repository.ItemSearchCond;
+import hello.item.repository.ItemUpdateDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+@Mapper
+public interface ItemMapper {
+    void save(Item item);
+
+    void update(@Param("id") Long id, @Param("updateParam")ItemUpdateDto updateParam);
+
+    Optional<Item> findById(Long id);
+
+    List<Item> findAll(ItemSearchCond itemSearch);
+}
